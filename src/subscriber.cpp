388ae739be1d4c59abd202006804ba0d7dd72ba8
Aditya Jadhav <adi30jadhav@gmail.com>
2021-11-01 01:32:24 -0400

    Added Copyright to Subscriber file

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file subscriber.cpp
+ * @author Aditya Jadhav (amjadhav@umd.edu)
+ * @brief Listener File
+ * @version 0.1
+ * @date 2021-10-31
+ * 
+ * @copyright Copyright (c) 2021
+ */
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
